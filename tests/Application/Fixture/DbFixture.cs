583b6efbd5d55407c6cb99eae4a417205f02211c
IcyMaster <icymaster2020@gmail.com>
2024-10-27 22:31:05 +0330

    write first test :D

---
+++ tests/Application/Fixture/DbFixture.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PersonalShop.Data;
+
+namespace PersonalShop.Tests.Application.Fixture
+{
+    public class DbFixture : IDisposable
+    {
+        public ApplicationDbContext DbContext { get; private set; }
+        private bool _disposed;
+
+        public DbFixture()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()).Options;
+
+            DbContext = new ApplicationDbContext(options);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                DbContext.Dispose();
+            }
+
+            _disposed = true;
+        }
+    }
+}
---
+++ tests/Application/Services/ProductService/ProductServiceTests.cs
@@ -0,0 +1,48 @@
+using Bogus;
+using MassTransit;
+using Moq;
+using PersonalShop.Data;
+using PersonalShop.Data.Repositories;
+using PersonalShop.Domain.Products.Dtos;
+using PersonalShop.Features.Product;
+using PersonalShop.Tests.Application.Fixture;
+
+
+namespace PersonalShop.Tests.Application.Services.ProductServiceTests
+{
+    public class ProductServiceTests:IClassFixture<DbFixture>
+    {
+        private readonly DbFixture _fixture;
+        private readonly UnitOfWork _unitOfWork;
+        private readonly ProductRepository _productRepo;
+
+        public ProductServiceTests(DbFixture fixture)
+        {
+            _fixture = fixture;
+            _unitOfWork = new(_fixture.DbContext);
+            _productRepo = new(_fixture.DbContext);
+        }
+
+        [Fact]
+        public async void AddProduct_Should_Return_True_When_ProductAdded()
+        {
+            //Arrange
+            var mockBus = new Mock<IBus>();
+            var productService = new ProductService(_productRepo,_unitOfWork, mockBus.Object);
+
+            var userId = Guid.NewGuid().ToString();
+
+            var productDto = new Faker<CreateProductDto>()
+                .RuleFor(x => x.Name, f => f.Commerce.ProductName())
+                .RuleFor(x => x.Name, f => f.Commerce.ProductDescription())
+                .RuleFor(p => p.Price, f => decimal.Parse(f.Commerce.Price(2000,1000000)));
+
+
+            //Act
+            var result = await productService.CreateProductByUserIdAsync(productDto, userId);
+
+            //Assert
+            Assert.True(result.IsSuccess);
+        }
+    }
+}
